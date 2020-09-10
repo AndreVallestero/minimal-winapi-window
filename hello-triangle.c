6347aa70d1ac3e7b89c88590c09045aca98f65ef
Iron Oxidizer <ironoxidizer@gmail.com>
2020-09-10 17:35:17 -0400

    Shortened minimum example add hello triangle

--- hello-triangle.c
+++ hello-triangle.c
@@ -2,11 +2,15 @@
 
 #include <windows.h>
 
+POINT *p_tri_points;
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch(msg) {
         case WM_CLOSE:
             PostQuitMessage(0);
             break;
+        case WM_PAINT:
+            Polygon(GetDC(hWnd), p_tri_points, 3);
         default:
             return DefWindowProc(hWnd, msg, wParam, lParam);
     }
@@ -29,8 +33,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                      CW_USEDEFAULT, 640, 360, NULL, NULL, hInstance, NULL))
         return 2;
 
+    p_tri_points = malloc(sizeof(POINT)*8);
+    p_tri_points[0].x = p_tri_points[0].y = p_tri_points[1].y = 10;
+    p_tri_points[1].x = p_tri_points[2].y = 110;
+    p_tri_points[2].x = 60;
+
     while(GetMessage(&msg, NULL, 0, 0 ) > 0)
         DispatchMessage(&msg);
+
+    free(p_tri_points);
 
     return 0;
 }
